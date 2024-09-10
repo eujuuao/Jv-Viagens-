f6f645263d5ad90aecbf739ccf58a0fb9bec6c89
eujuuao <joaovictorgoncalvesoliveira450@gmail.com>
2024-09-09 23:29:45 -0300

    finalziando confirguração da biblioteca prisma para conectar o banco de dados mongo db

--- server.mjs
+++ server.mjs
@@ -1,9 +1,20 @@
 import express from 'express'
 
 const app = express ()
+app.use(express.json())
+
+const users = []
+
+app.post('/usuarios', (req, res) => {
+
+    users.push(req.body)
+
+    res.status(201).json(req.body)
+
+})
 
 app.get('/usuarios', (req, res) => {
-    res.send('ok, deu bom')
+    res.status(200).json(users)
 })
 
 app.listen(3000)
