8c610543f030bc168a4b05fdc29ed4583f7717f9
eujuuao <joaovictorgoncalvesoliveira450@gmail.com>
2024-09-17 00:10:02 -0300

    configurado a rota delte

--- server.mjs
+++ server.mjs
@@ -6,7 +6,6 @@ const prisma = new PrismaClient()
 const app = express ()
 app.use(express.json())
 
-const users = []
 
 app.post('/usuarios', async (req, res) => {
 
@@ -19,11 +18,39 @@ app.post('/usuarios', async (req, res) => {
     })
 
     res.status(201).json(req.body)
-
 })
 
-app.get('/usuarios', (req, res) => {
+app.get('/usuarios', async (req, res) => {
+    
+    const users = await prisma.user.findMany()
+    
     res.status(200).json(users)
+})
+
+app.put('/usuarios/:id', async (req, res) => {
+
+    await prisma.user.update({
+        where: {
+            id: req.params.id
+        },
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    })
+
+    res.status(201).json(req.body)
+})
+
+app.delete('/usuarios/:id', async (req, res) => {
+    await prisma.user.delete({
+        where: {
+            id: req.params.id
+        }
+    })
+
+    res.status(200).json({ message: ' Usuário deletado com sucesso! ' })
 })
 
 app.listen(3000)
